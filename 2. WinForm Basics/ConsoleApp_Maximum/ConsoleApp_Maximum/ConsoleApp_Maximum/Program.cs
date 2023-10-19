edb3f21de469061253e2f405628cf64630f6f051
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-10-19 18:13:47 +0200

    Add Console Project Maximum of 3 numbers

---
+++ 2. WinForm Basics/ConsoleApp_Maximum/ConsoleApp_Maximum/ConsoleApp_Maximum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+class Program
+{
+	static void Main()
+	{
+		WriteLine("Maximum von drei Zahlen");
+		WriteLine();
+		int a, b, c, max;
+		WriteLine("Geben Sie die erste Zahl ein:");
+		a = Convert.ToInt32(ReadLine());
+		max = a;
+		WriteLine("Geben Sie die zweite Zahl ein:");
+		b = Convert.ToInt32(ReadLine());
+		if (b > a)
+		{
+			max = b;
+		}
+
+		WriteLine("Geben Sie die dritte Zahl ein:");
+		c = Convert.ToInt32(ReadLine());
+		if (c > max)
+		{
+			max = c;
+		}
+		WriteLine("Das Maximum ist " + max.ToString());
+		ReadLine();
+	}
+}
