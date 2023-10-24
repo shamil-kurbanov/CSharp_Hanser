27a1dbe6089b39bde2e3c3201ed338a732ffb629
shamil-kurbanov <141030690+shamil-kurbanov@users.noreply.github.com>
2023-10-24 13:27:06 +0200

    Add new project Einfache Klasse and added class Kunde.cs

---
+++ 4. OOP/4.1 A short introduction to OOP/4. Einfache_Klasse/4. Einfache_Klasse/Kunde.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._Einfache_Klasse
+{
+	internal class Kunde
+	{
+		private string anrede; // Feld
+		private string name; // Feld
+
+		//Konstruktor
+		public Kunde(string anrede, string name)
+		{
+			this.anrede = anrede;
+			this.name = name;
+		}
+
+		//Eigenschaft
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		//Methode
+		public string Adresse()
+		{
+			string s = anrede + " " + Name;
+			return s;
+		}
+	}
+}
