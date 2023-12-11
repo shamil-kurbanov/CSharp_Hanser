05047fa63d997f294585fa11df3c2b731361ca7c
Shamil Kurbanov <kurbanov@outlook.de>
2023-12-11 17:15:19 +0100

    Add simple project with delegate and function

--- 6. Weitere_Sprachfeaures/6.6. Delegates/6.6.2_delegate_example/obj/Release/net8.0/6.6.2_delegate_example.AssemblyInfo.cs
+++ 6. Weitere_Sprachfeaures/6.6. Delegates/6.6.2_delegate_example/obj/Release/net8.0/6.6.2_delegate_example.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("6.8_MyArray")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("6.6.2_delegate_example")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("6.8_MyArray")]
-[assembly: System.Reflection.AssemblyTitleAttribute("6.8_MyArray")]
+[assembly: System.Reflection.AssemblyProductAttribute("6.6.2_delegate_example")]
+[assembly: System.Reflection.AssemblyTitleAttribute("6.6.2_delegate_example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Von der MSBuild WriteCodeFragment-Klasse generiert.
